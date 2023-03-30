47df74662093615182cd145055ec5032f5f58263
Williams Mata <williams.rm99@gmail.com>
2023-03-30 08:15:17 -0400

    pokemon cards done

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,19 +1,25 @@
-import React from "react";
 import Document, {
   Html,
   Head,
   Main,
   NextScript,
   DocumentContext,
+  DocumentInitialProps,
 } from "next/document";
+
 import { CssBaseline } from "@nextui-org/react";
 
-class MyDocument extends Document {
-  static async getInitialProps(ctx: DocumentContext) {
+import { Children } from "react";
+
+export default class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
     const initialProps = await Document.getInitialProps(ctx);
+
     return {
       ...initialProps,
-      styles: React.Children.toArray([initialProps.styles]),
+      styles: Children.toArray([initialProps.styles]),
     };
   }
 
@@ -29,5 +35,3 @@ class MyDocument extends Document {
     );
   }
 }
-
-export default MyDocument;
