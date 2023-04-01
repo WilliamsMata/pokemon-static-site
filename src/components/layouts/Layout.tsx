064aa8a9fad6f666f17e28ddfd4dfb9ba9cd6569
Williams Mata <williams.rm99@gmail.com>
2023-04-01 13:47:16 -0400

    open graph meta tags added

--- src/components/layouts/Layout.tsx
+++ src/components/layouts/Layout.tsx
@@ -6,6 +6,8 @@ interface Props {
   title?: string;
 }
 
+const origin = typeof window === "undefined" ? "" : window.location.origin;
+
 export const Layout = ({ title, children }: PropsWithChildren<Props>) => {
   return (
     <>
@@ -17,6 +19,13 @@ export const Layout = ({ title, children }: PropsWithChildren<Props>) => {
           content={`Información sobre el pokemon ${title}`}
         />
         <meta name="keywords" content={`${title}, pokemon, pokedex`} />
+
+        <meta property="og:title" content={`Información sobre ${title}`} />
+        <meta
+          property="og:description"
+          content={`Esta es la pagina sobre ${title}`}
+        />
+        <meta property="og:image" content={`${origin}/img/banner.webp`} />
       </Head>
 
       <NavbarComponent />
