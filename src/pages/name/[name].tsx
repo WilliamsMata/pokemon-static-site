dddca8076ad3839f2953c59b5733e2c74bc80815
Williams Mata <williams.rm99@gmail.com>
2023-03-31 21:57:00 -0400

    component Props optimized

--- src/pages/name/[name].tsx
+++ src/pages/name/[name].tsx
@@ -5,14 +5,14 @@ import confetti from "canvas-confetti";
 
 import { Layout } from "@/components/layouts";
 import { pokeApi } from "@/api";
-import { Pokemon } from "@/interfaces";
+import { Pokemon, PokemonListResponse } from "@/interfaces";
 import { existInFavorites, toggleFavorites } from "@/utils";
 
 interface Props {
   pokemon: Pokemon;
 }
 
-const PokemonPage: NextPage<Props> = ({ pokemon }) => {
+const PokemonByNamePage: NextPage<Props> = ({ pokemon }) => {
   const [isInFavorites, setIsInFavorites] = useState<boolean>(
     existInFavorites(pokemon.id)
   );
@@ -119,20 +119,21 @@ const PokemonPage: NextPage<Props> = ({ pokemon }) => {
 };
 
 export const getStaticPaths: GetStaticPaths = async (ctx) => {
-  const pokemons151 = [...Array(151)].map((value, index) => `${index + 1}`);
+  const { data } = await pokeApi.get<PokemonListResponse>("pokemon?limit=151");
+  const pokemons151 = data.results;
 
   return {
-    paths: pokemons151.map((id) => ({
-      params: { id },
+    paths: pokemons151.map(({ name }) => ({
+      params: { name },
     })),
     fallback: false,
   };
 };
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
-  const { id } = params as { id: string };
+  const { name } = params as { name: string };
 
-  const { data } = await pokeApi.get<Pokemon>(`pokemon/${id}`);
+  const { data } = await pokeApi.get<Pokemon>(`pokemon/${name}`);
 
   const pokemon = {
     id: data.id,
@@ -147,4 +148,4 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   };
 };
 
-export default PokemonPage;
+export default PokemonByNamePage;
